513d2e1031dfc71092667afd8bf724e7b99de3df
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-02 16:01:55 +0530

    Add files via upload

---
+++ Assignment No 2/01_Even Or Odd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No =0;
+
+    printf("\n Enter a Number :");
+    scanf("%d",&No);
+
+    if(No % 2 ==0)
+    {
+        printf("\n Given Number Is Even");
+    }
+    else
+    {
+        printf("\n Given Number Is Odd");
+    }
+        printf("\n \nThanks For Visiting..!!");
+
+    getch();
+    return 0;
+}
