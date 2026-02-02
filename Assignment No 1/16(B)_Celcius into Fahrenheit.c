56270db785a48586740ae324178045b6480ff069
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-02 15:35:45 +0530

    Add files via upload

---
+++ Assignment No 1/16(B)_Celcius into Fahrenheit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Fhrt = 0.0, Cel = 0.0;
+
+    printf("\n Enter Temprature in Celcius :");
+    scanf("%f",&Cel);
+
+   Fhrt =(9.0/5.0)* Cel + 32.0 ;
+
+   printf("\n Temprature in Celcius into Fharnite is = %0.4f",Fhrt);
+
+   getch();
+   return 0;
+
+}
