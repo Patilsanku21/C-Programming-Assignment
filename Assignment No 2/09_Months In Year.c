4c1ca4fa2d24ebfa5f87d3361e7886eb1f94df86
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-04 20:51:31 +0530

    Add files via upload

---
+++ Assignment No 2/09_Months In Year.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Month = 0;
+
+    printf("\n Enter The Number :");
+    scanf("%d",&Month);
+
+    if(Month == 1)
+    {
+        printf("\n The Month Is January");
+    }
+    else if(Month == 2)
+    {
+        printf("\n The Month Is Februuary");
+    }
+    else if(Month == 3)
+    {
+        printf("\n The Month Is March");
+    }
+    else if(Month == 4)
+    {
+        printf("\n The Month Is April");
+    }
+    else if(Month == 5)
+    {
+        printf("\n The Month Is May");
+    }
+    else if(Month == 6)
+    {
+        printf("\n The Month Is June");
+    }
+    else if(Month == 7)
+    {
+        printf("\n The Month Is July");
+    }
+    else if(Month == 8)
+    {
+        printf("\n The Month Is August");
+    }
+    else if(Month == 9)
+    {
+        printf("\n The Month Is September");
+    }
+    else if(Month == 10)
+    {
+        printf("\n The Month Is October");
+    }
+    else if(Month == 11)
+    {
+        printf("\n The Month Is November");
+    }
+    else if(Month == 12)
+    {
+        printf("\n The Month Is December");
+    }
+    else
+    {
+        printf("\n Month Is Not Valid");
+    }
+        printf("\n \nThanks For Visiting...!!");
+
+        getch();
+        return 0;
+}
