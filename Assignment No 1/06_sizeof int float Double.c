af2f1b7ea048c6a95c3ef26c76309823e757880d
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-10 22:11:48 +0530

    Add files via upload

---
+++ Assignment No 1/06_sizeof int float Double.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int ;
+    float;
+    double;
+
+    printf("\n int %d byte ",sizeof(int));
+    printf("\n float %d byte",sizeof(float));
+    printf("\n double %d byte",sizeof(double));
+
+    getch();
+    return 0;
+}
