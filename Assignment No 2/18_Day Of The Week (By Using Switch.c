6516a3f1599dfd735e00333a93138b076ad90eed
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-11 22:24:37 +0530

    Add files via upload

---
+++ Assignment No 2/18_Day Of The Week (By Using Switch.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int  Day = 0;
+
+    printf("\n Enter a Day Number:");
+    scanf("%d",&Day);
+
+    switch(Day)
+    {
+        case 1:
+
+            printf("\n Day In The Week Monday");
+            break;
+
+        case 2:
+
+            printf("\n Day In The Week Tuesday");
+            break;
+
+        case 3:
+
+            printf("\n Day In The Week Wednesay");
+            break;
+
+        case 4:
+
+            printf("\n Day In The Week Thursday");
+            break;
+
+        case 5:
+
+            printf("\n Day In The Week Friday");
+            break;
+
+        case 6:
+
+            printf("\n Day In The Week Saturday");
+            break;
+
+        case 7:
+
+            printf("\n Day In The Week Sunday");
+            break;
+
+        default:
+
+            printf("\n Invalid Day");
+            break;
+        }
+            printf("\n\n Thank You..!!");
+         getch();
+         return 0;
+
+    }
+
