2130a7b1e4de59568800e93bf3bc16e8cca98051
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-14 10:59:20 +0530

    Add files via upload

---
+++ Assignment No 3/01(A)_Write a Program That Uses a Loop To Print The Numbers From 101 to 110.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 101;
+
+    printf("\n\n print 101 to 110 Numbers");
+
+    while(i <= 110)
+    {
+        printf("\n %d",i);
+        i++;
+    }
+
+        printf("\n\n Thank You..!!");
+
+        getch();
+        return 0;
+}
