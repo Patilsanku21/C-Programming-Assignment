e90529e6206677a21d4ca001bc8a9a5d5d650e59
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-14 11:00:37 +0530

    Add files via upload

---
+++ Assignment No 3/01(D)_Write a Program That Uses a Loop To Print The Numbers From 101 to 110(Reverse).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0;
+
+    printf("\n Print 101 To 110 Numbers:");
+
+    for(i = 101 ; i<=110 ;i++)
+    {
+        printf("\n %d",i);
+    }
+        printf("\n\n Thank You..!!");
+
+        getch();
+        return 0;
+}
