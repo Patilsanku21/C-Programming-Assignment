fdcbf61f050faeeb9f78a64be78bbee70d4b0405
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-18 21:28:30 +0530

    Add files via upload

---
+++ Pattern Printing/07_Write A program To Display H star Pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0,j = 0, RC = 0;
+
+    printf("\n Enter Row And Column:");
+    scanf("%d",&RC);
+
+    for(i=1;i<=RC;i++)
+    {
+        for(j=1;j<= RC;j++)
+        {
+            if( j==1 ||i==(RC+2)/2|| j==RC)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+
+        }
+             printf("\n");
+    }
+        getch();
+
+        return 0;
+}
