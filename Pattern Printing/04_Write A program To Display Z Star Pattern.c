266af78798334219f8e774fe016c820e90aecf43
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-18 21:25:13 +0530

    Add files via upload

---
+++ Pattern Printing/04_Write A program To Display Z Star Pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0,j = 0,RC = 0;
+
+     printf("\n Enter a Row And Column:");
+     scanf("%d",&RC);
+
+     for(i =1; i<= RC ;i++)
+     {
+        for(j=1; j<=RC; j++)
+        {
+            if(i==1|| j+i==RC+1 || i==RC)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+         printf("\n");
+        }
+
+
+     getch();
+     return 0;
+}
