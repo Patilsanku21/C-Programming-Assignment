f2b33f0b8c104f26eac1e39f1169fc66192c31f2
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-23 09:58:29 +0530

    Add files via upload

---
+++ Bitwise Operators/03_Bitwise OR.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int N1 = 0, N2 = 0,Res =0;
+
+   printf("\n Enter a Number :");
+   scanf("%d",&N1);
+
+   printf("\n Enter a Number :");
+   scanf("%d",&N2);
+
+    Res = N1 | N2 ;
+
+   printf("\n Result = %d | %d = %d.",N1,N2,Res);
+
+   getch();
+   return 0;
+}
