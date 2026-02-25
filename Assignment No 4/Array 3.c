1fd2f94f45239fc8491126edc6b02fd2f4c8bbe3
Sanskruti Patil <sanskrutip438@gmail.com>
2026-02-25 22:09:02 +0530

    Add files via upload

---
+++ Assignment No 4/Array 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int value[7]={0,0,0,0,0,0,0};
+
+    printf("\n Value Of First Element = %d",value[0]);
+    printf("\n Value Of Second  Element = %d",value[1]);
+    printf("\n Value Of Third Element = %d",value[2]);
+    printf("\n Value Of Fourth  Element = %d",value[3]);
+    printf("\n Value Of Fifth Element = %d",value[4]);
+    printf("\n Value Of Sixth Element = %d",value[5]);
+    printf("\n Value Of seventh Element = %d",value[6]);
+
+    getch();
+    return 0;
+}
