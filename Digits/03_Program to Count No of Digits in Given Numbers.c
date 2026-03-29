e8f7e583a24d2b96cc5726310664a770bce91801
Sanskruti Patil <sanskrutip438@gmail.com>
2026-03-29 20:17:43 +0530

    Add files via upload

---
+++ Digits/03_Program to Count No of Digits in Given Numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Dcnt = 0,Temp = 0;
+
+    printf("\n Enter A Number :");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    while(Temp > 0)
+    {
+        Temp = Temp / 10;
+
+        Dcnt++;
+
+    }
+
+    printf("\nDigits count in Given Number %d = %d",No, Dcnt);
+
+    getch();
+    return 0;
+}
