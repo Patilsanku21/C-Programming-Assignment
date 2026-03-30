c91a81c2479585726f7097479d55ead575d65e40
Sanskruti Patil <sanskrutip438@gmail.com>
2026-03-30 10:51:07 +0530

    Add files via upload

---
+++ Digits/06_Program To Count No Of Zero Digit Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0 ,Zcnt = 0,Temp = 0,Dig = 0;
+
+    printf("\n Enter A Number :");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10 ;
+
+        if(Dig == 0)
+        {
+            Zcnt++;
+        }
+        Temp = Temp / 10;
+    }
+
+    printf("\n Zero Count  in Given Number %d =%d",No ,Zcnt);
+
+    getch();
+    return 0;
+
+    }
