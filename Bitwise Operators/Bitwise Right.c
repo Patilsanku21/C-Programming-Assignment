42763ccf0f43a9b301e3beebce4e8055d6022f3c
Sanskruti Patil <sanskrutip438@gmail.com>
2026-05-20 11:56:10 +0530

    Add files via upload

---
+++ Bitwise Operators/Bitwise Right.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0,Res = 0;
+
+    printf("\n Enter A First Number:");
+    scanf("%d",&No);
+
+    Res = No >> 9;
+
+    printf(" Result %d >> 9 = %d",No,Res);
+
+    getch();
+    return 0;
+}
