1ba28f4f45ebebd653557db4a8c0b8d2cb65095b
Sanskruti Patil <sanskrutip438@gmail.com>
2026-05-20 12:37:07 +0530

    Add files via upload

---
+++ Digits/Digit Using Function/library/14(A).c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include "lib.h"
+
+short int Dsum(int No);
+
+int main()
+{
+        short int Cnt = 0;
+        int No = 0;
+
+        printf("\n Enter A Number :");
+        scanf("%d",&No);
+
+        Cnt = Dsum(No);
+
+        printf("\n Addition Of Given Number is %d",Cnt);
+
+        getch();
+
+        return 0;
+}
+
+        short int Dsum(int No)
+{
+
+
+         short int dCnt = 0;
+
+         while(No != 0)
+         {
+            dCnt++;
+
+            No /= 10;
+         }
+
+         return dCnt;
+
+}
