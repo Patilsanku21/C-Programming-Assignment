b3b501ae63373bc9ce464f01f4c1b510b679559b
Sanskruti Patil <sanskrutip438@gmail.com>
2026-05-23 11:09:07 +0530

    Add files via upload

---
+++ Bitwise Operators/Bitwise A/03_program to Check Whether 15th bit of given Number is ON Or OFF.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0,BNo = 0;
+
+    printf("\n Enter a Number :");
+    scanf("%d",&No);
+
+    printf("\n Enter a Bit Number :");
+    scanf("%d",&BNo);
+
+    if(BNo > 32 || BNo < 1)
+    {
+        printf("\n Invalid Bit Number \n");
+    }
+    if(BNo > 32)
+    {
+
+    }
+}
